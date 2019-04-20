25eee7484fb2c192bca81a288611779674384294
timotheein <easer.ceo@gmail.com>
2019-04-20 02:11:07 +0200

    Login activity + Home + Why FB + font is OK +Added the ViewPager + Added a swiper on Test... seems to work

--- Activities/Test.js
+++ Activities/Test.js
@@ -1,52 +1,130 @@
 import React from 'react'
-import { StyleSheet, View, Text,  } from 'react-native'
+import { StyleSheet, View, Image,Text,TouchableOpacity  } from 'react-native'
 import {IndicatorViewPager,PagerTabIndicator, PagerTitleIndicator, PagerDotIndicator } from 'rn-viewpager'
+import CardStack, { Card } from 'react-native-card-stack-swiper';
+
 
 class Test extends React.Component {
-
-
-    _renderTabIndicator() {
-        let tabs = [{
-            iconSource: require('../Images/Login/icone_map.png'),
-            selectedIconSource: require('../Images/Login/icone_map.png'),
-        },{
-            iconSource: require('../Images/Login/fire.png'),
-            selectedIconSource: require('../Images/Login/fire.png'),
-
-
-        },{
-            iconSource: require('../Images/Login/message.png'),
-            selectedIconSource: require('../Images/Login/message.png'),
-
-        }];
-        return <PagerTabIndicator tabs={tabs} />;
-    }
-
     render() {
         return (
             <View style={{flex:1}}>
-                <IndicatorViewPager
-                    style={{flex:1, paddingTop:20, backgroundColor:'white'}}
-                    indicator={this._renderTabIndicator()}
+                <CardStack
+                    style={styles.content}
+                    renderNoMoreCards={() => <Text style={{fontWeight:'700', fontSize:18, color:'gray'}}>No more cards :(</Text>}
+                    ref={swiper => {
+                        this.swiper = swiper
+                    }}
+                    onSwiped={() => console.log('onSwiped')}
+                    onSwipedLeft={() => console.log('onSwipedLeft')}
                 >
-                    <View style={{backgroundColor:'cadetblue'}}>
-                        <Text>page one</Text>
+                    <Card style={[styles.card, styles.card1]}><Text style={styles.label}>A</Text></Card>
+                    <Card style={[styles.card, styles.card2]} onSwipedLeft={() => alert('onSwipedLeft')}><Text style={styles.label}>B</Text></Card>
+                    <Card style={[styles.card, styles.card1]}><Text style={styles.label}>C</Text></Card>
+                    <Card style={[styles.card, styles.card2]}><Text style={styles.label}>D</Text></Card>
+                    <Card style={[styles.card, styles.card1]}><Text style={styles.label}>E</Text></Card>
+                </CardStack>
+
+                <View style={styles.footer}>
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={[styles.button,styles.red]} onPress={()=>{
+                            this.swiper.swipeLeft();
+                        }}>
+                            <Image source={require('../Images/Login/icone_map.png')} resizeMode={'contain'} style={{ height: 62, width: 62 }} />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.button,styles.orange]} onPress={() => {
+                            this.swiper.goBackFromLeft();
+                        }}>
+                            <Image source={require('../Images/Login/icone_map.png')} resizeMode={'contain'} style={{ height: 32, width: 32, borderRadius: 5 }} />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.button,styles.green]} onPress={()=>{
+                            this.swiper.swipeRight();
+                        }}>
+                            <Image source={require('../Images/Login/icone_map.png')} resizeMode={'contain'} style={{ height: 62, width: 62 }} />
+                        </TouchableOpacity>
                     </View>
-                    <View style={{backgroundColor:'cornflowerblue'}}>
-                        <Text>page two</Text>
-                    </View>
-                    <View style={{backgroundColor:'#1AA094'}}>
-                        <Text>page three</Text>
-                    </View>
-                </IndicatorViewPager>
+                </View>
+
             </View>
-        )
+        );
     }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
+    container: {
         flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#f2f2f2',
+    },
+    content:{
+        flex: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    card:{
+        width: 320,
+        height: 470,
+        backgroundColor: '#FE474C',
+        borderRadius: 5,
+        shadowColor: 'rgba(0,0,0,0.5)',
+        shadowOffset: {
+            width: 0,
+            height: 1
+        },
+        shadowOpacity:0.5,
+    },
+    card1: {
+        backgroundColor: '#FE474C',
+    },
+    card2: {
+        backgroundColor: '#FEB12C',
+    },
+    label: {
+        lineHeight: 400,
+        textAlign: 'center',
+        fontSize: 55,
+        color: '#ffffff',
+        backgroundColor: 'transparent',
+    },
+    footer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    buttonContainer:{
+        width:220,
+        flexDirection:'row',
+        justifyContent: 'space-between',
+    },
+    button:{
+
+        shadowOpacity:0.5,
+        backgroundColor:'#fff',
+        alignItems:'center',
+        justifyContent:'center',
+        zIndex: 0,
+    },
+    orange:{
+        width:55,
+        height:55,
+        borderWidth:4,
+        borderRadius:55,
+        marginTop:-15
+    },
+    green:{
+        width:75,
+        height:75,
+        backgroundColor:'#fff',
+        borderRadius:75,
+        borderWidth:6,
+        borderColor:'#01df8a',
+    },
+    red:{
+        width:75,
+        height:75,
+        backgroundColor:'#fff',
+        borderRadius:75,
+        borderWidth:6,
+        borderColor:'#fd267d',
     }
 })
 
