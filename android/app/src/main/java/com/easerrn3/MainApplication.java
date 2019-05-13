edac38d1857efc1865a27547cb10a6afba960ae6
timotheein <easer.ceo@gmail.com>
2019-05-13 15:54:45 +0200

    Login activity + Home + Why FB + font is OK +Added the ViewPager + Added a swiper on Profils screen + Added a map
    ->going to add symbols for tabs

--- android/app/src/main/java/com/easerrn3/MainApplication.java
+++ android/app/src/main/java/com/easerrn3/MainApplication.java
@@ -3,6 +3,7 @@ package com.easerrn3;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,8 +24,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNGestureHandlerPackage()
+              new MainReactPackage(),
+              new RNGestureHandlerPackage(),
+              new MapsPackage()
+              //  new RNFusedLocationPackage()
       );
     }
 
